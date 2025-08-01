56dd347e2ec4a82bbbed53d34c2bec8999ae7c01
Seralyn <seralyn84@gmail.com>
2025-08-01 19:33:51 +0200

    at least the red LED works, inexplicably :/

--- TN_01_firmware_v002-LEO/TN_01_firmware_v002-LEO.ino
+++ TN_01_firmware_v002-LEO/TN_01_firmware_v002-LEO.ino
@@ -1,45 +1,54 @@
 #include <Wire.h>
-#include <AS5600.h>
+//#include <AS5600.h>
 
 // AS5600 setup
-AS5600 encoder;
-int lastHEValScaled = -1;
-unsigned long lastHEPrintTime = 0;
+//AS5600 encoder;
+//int lastHEValScaled = -1;
+//unsigned long lastHEPrintTime = 0;
 
 // digital pin definitions
-int greenLED = 2;
-int redLED = 3;
+int greenLED = 12;
+int redLED = 11;
 const int bIPB_led = 4;
 const int bIPB = 5;
-const int illumRocker = 6;
+// const int illumRocker = 6;
 const int mtlThrw = 7;
-const int ignition = 8;
+// const int ignition = 8;
 const int pbMomSw = 9;
 
 // analog pin definitions
-const int linPot = A0;
-const int rotPot = A1;
+// const int linPot = A0;
+// const int rotPot = A1;
 
 // variables to track last pot values and print times
-int lastLinPotValScaled = -1;
-unsigned long lastLinPotPrintTime = 0;
+// int lastLinPotValScaled = -1;
+// unsigned long lastLinPotPrintTime = 0;
 
-int lastRotPotValScaled = -1;
-unsigned long lastRotPotPrintTime = 0;
+// int lastRotPotValScaled = -1;
+// unsigned long lastRotPotPrintTime = 0;
 
 void setup() {
   pinMode(pbMomSw, INPUT_PULLUP);
-  pinMode(ignition, INPUT_PULLUP);
+  // pinMode(ignition, INPUT_PULLUP);
   pinMode(mtlThrw, INPUT_PULLUP);
   pinMode(bIPB, INPUT_PULLUP);
-  pinMode(illumRocker, INPUT);
+  // pinMode(illumRocker, INPUT);
 
-  digitalWrite(redLED, HIGH);
-  digitalWrite(bIPB_led, LOW);
-
+  pinMode(greenLED, OUTPUT);
+  pinMode(redLED, OUTPUT);
+  pinMode(bIPB_led, OUTPUT);
+  
   Serial.begin(9600);
+  while (!Serial);
   Wire.begin();
 
+
+  digitalWrite(greenLED, LOW);  // Ensure green LED is off at boot
+  digitalWrite(redLED, HIGH);   // Ensure red LED is on at boot
+  digitalWrite(bIPB_led, LOW);  // Ensure big button LED is off
+
+
+  /*
   if (!encoder.begin()) {
     Serial.println("AS5600 not detected. Check wiring.");
     while (1);
@@ -48,67 +57,89 @@ void setup() {
   }
 
   encoder.setDirection(AS5600_CLOCK_WISE); // Or COUNTERCLOCK_WISE if needed
+  */
 }
 
 void loop() {
   int msButtonState = digitalRead(pbMomSw);
-  int ignitionState = digitalRead(ignition);
+  // int ignitionState = digitalRead(ignition);
   int mtlThrwState = digitalRead(mtlThrw);
   int bIPBState = digitalRead(bIPB);
-  int illumRockerState = digitalRead(illumRocker);
-  int linPotVal = analogRead(linPot);
-  int rotPotVal = analogRead(rotPot);
+  // int illumRockerState = digitalRead(illumRocker);
+  // int linPotVal = analogRead(linPot);
+  // int rotPotVal = analogRead(rotPot);
 
-  int linPotValScaled = map(linPotVal, 0, 1023, 0, 100);
-  int rotPotValScaled = map(rotPotVal, 159, 597, 0, 100);
+  // int linPotValScaled = map(linPotVal, 0, 1023, 0, 100);
+  // int rotPotValScaled = map(rotPotVal, 159, 597, 0, 100);
 
   // Read and scale AS5600 angle
+  /*
   int rawHEVal = encoder.readAngle();
   rawHEVal = constrain(rawHEVal, 1404, 2280); // safety net
   int heValScaled = map(rawHEVal, 1404, 2280, 0, 100);
+  */
 
-  unsigned long now = millis();
+  // unsigned long now = millis();
 
-  if (linPotValScaled != lastLinPotValScaled && (now - lastLinPotPrintTime >= 500)) {
-    Serial.print("Linear Pot Value: ");
-    Serial.println(linPotValScaled);
-    lastLinPotValScaled = linPotValScaled;
-    lastLinPotPrintTime = now;
-  }
+  // if (linPotValScaled != lastLinPotValScaled && (now - lastLinPotPrintTime >= 500)) {
+  //   Serial.print("Linear Pot Value: ");
+  //   Serial.println(linPotValScaled);
+  //   lastLinPotValScaled = linPotValScaled;
+  //   lastLinPotPrintTime = now;
+  // }
 
-  if (rotPotValScaled != lastRotPotValScaled && (now - lastRotPotPrintTime >= 500)) {
-    Serial.print("Adjusted Rotary Pot Value: ");
-    Serial.println(rotPotValScaled);
-    lastRotPotValScaled = rotPotValScaled;
-    lastRotPotPrintTime = now;
-  }
+  // if (rotPotValScaled != lastRotPotValScaled && (now - lastRotPotPrintTime >= 500)) {
+  //   Serial.print("Adjusted Rotary Pot Value: ");
+  //   Serial.println(rotPotValScaled);
+  //   lastRotPotValScaled = rotPotValScaled;
+  //   lastRotPotPrintTime = now;
+  // }
 
+  /*
   if (heValScaled != lastHEValScaled && (now - lastHEPrintTime >= 500)) {
     Serial.print("Hall Sensor (AS5600) Value: ");
     Serial.println(heValScaled);
     lastHEValScaled = heValScaled;
     lastHEPrintTime = now;
   }
+  */
 
   if (msButtonState == LOW) {
     Serial.println("Momentary Switch (small) pressed!");
-  } else if (ignitionState == LOW) {
-    Serial.println("ignition is ON");
-  } else if (mtlThrwState == LOW) {
-    Serial.println("Metal Switch ON");
-    digitalWrite(redLED, LOW);
-    digitalWrite(greenLED, HIGH);
-  } else if (illumRockerState == HIGH) {
-    Serial.println("rocker activated");
-    delay(800);
-  } else if (bIPBState == LOW) {
+  } 
+  // else if (ignitionState == LOW) {
+  //   Serial.println("ignition is ON");
+  // } 
+
+  // else if (illumRockerState == HIGH) {
+  //   Serial.println("rocker activated");
+  //   delay(800);
+  // } 
+  else if (bIPBState == LOW) {
     digitalWrite(bIPB_led, HIGH);
     Serial.println("Big Switch Pressed!");
-  } else {
-    digitalWrite(greenLED, LOW);
-    digitalWrite(redLED, HIGH);
+  } 
+  else {
     digitalWrite(bIPB_led, LOW);
   }
+
+  Serial.print("mtlThrwState: ");
+  Serial.println(mtlThrwState);
+
+  Serial.print("Setting redLED to: ");
+  Serial.println(mtlThrwState == LOW ? "LOW (OFF)" : "HIGH (ON)");
+
+  Serial.print("Setting greenLED to: ");
+  Serial.println(mtlThrwState == LOW ? "HIGH (ON)" : "LOW (OFF)");
+
+  if (mtlThrwState == LOW) {
+  Serial.println("Metal Switch ON");
+  digitalWrite(redLED, LOW);
+  digitalWrite(greenLED, HIGH);
+} else {
+  digitalWrite(redLED, HIGH);
+  digitalWrite(greenLED, LOW);
+}
 
   delay(100);
 }
